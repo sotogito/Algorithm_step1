1ab3ce7dac18ad6987205d17319d984240972888
sotogito <108535526+sotogito@users.noreply.github.com>
2024-06-08 14:12:03 +0900

    3120 resolve

--- 그리디/3120 : 리모컨.py
+++ 그리디/3120 : 리모컨.py
@@ -6,19 +6,37 @@ tenBtn = 10
 
 nowTemperature, targetTemperature = map(int, input().split())
 
-changeTemperature = targetTemperature - nowTemperature
-if changeTemperature < 0:
-    changeTemperature = -changeTemperature
+changeTemperature = abs(targetTemperature - nowTemperature)
 
-tenBtnCount = int(changeTemperature / tenBtn)
-changeTemperature -= tenBtnCount * tenBtn
+count = 0
 
-fiveBtnCount = int(changeTemperature / fiveBtn)
-changeTemperature -= fiveBtnCount * fiveBtn
+while True:
+    if changeTemperature == 0:
+        break
 
-oneBtnCount = int(changeTemperature / oneBtn)
-changeTemperature -= oneBtnCount * oneBtn
+    tenBtnCount = int(changeTemperature / tenBtn)
+    count += tenBtnCount
+    changeTemperature -= tenBtnCount * tenBtn
 
-count = tenBtnCount + fiveBtnCount + oneBtnCount
+    if changeTemperature != 0:
+        distance_to_0 = abs(changeTemperature - 1)
+        distance_to_5 = abs(changeTemperature - 5)
+        distance_to_10 = abs(changeTemperature - 10)
+
+        min_distance = min(distance_to_0, distance_to_5, distance_to_10)
+
+        if min_distance == distance_to_0:
+            changeTemperature = abs(changeTemperature - oneBtn)
+        elif min_distance == distance_to_5:
+            changeTemperature = abs(changeTemperature - fiveBtn)
+        elif min_distance == distance_to_10:
+            changeTemperature = abs(changeTemperature - tenBtn)
+
+        count += 1
+
+        oneBtnCount = int(changeTemperature / oneBtn)
+        count += oneBtnCount
+        changeTemperature -= oneBtnCount * oneBtn
 
 print(count)
+
