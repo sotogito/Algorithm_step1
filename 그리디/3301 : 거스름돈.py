aaf2c1a04a4cb371188134c0bc1192d311f9e07f
sotogito <108535526+sotogito@users.noreply.github.com>
2024-06-08 15:14:16 +0900

    docs (3301) : 파이썬 함수 리턴값 메모

--- 그리디/3301 : 거스름돈.py
+++ 그리디/3301 : 거스름돈.py
@@ -9,10 +9,15 @@ count = 0
 def calculate(count, money, change):
     count += money // change
     money -= (money // change) * change
-    return count, money
+    return count, money ###
 
 
 for change in changes:
-    count, money = calculate(count, money, change)
+    count, money = calculate(count, money, change) ###
 
 print(count)
+
+"""
+파이썬 함수는 2개 객체를 리턴할 수 있다.
+리턴해야 위의 변수에 누적된다. 이건 자바도 마찬가지
+"""
