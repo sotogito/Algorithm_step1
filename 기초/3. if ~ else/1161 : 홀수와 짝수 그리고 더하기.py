74993240ff9a15b322c863836e2d76a26b230f0a
sotogito <108535526+sotogito@users.noreply.github.com>
2024-06-10 01:12:39 +0900

    solve(1161)

---
+++ 기초/3. if ~ else/1161 : 홀수와 짝수 그리고 더하기.py
@@ -0,0 +1,15 @@
+f, t = map(int, input().split())
+
+
+def evenOrOdd(a):
+    if a % 2 == 0:
+        return "짝수"
+    else:
+        return "홀수"
+
+
+result = evenOrOdd(f + t)
+f = evenOrOdd(f)
+t = evenOrOdd(t)
+
+print(f + "+" + t + "=" + result)
